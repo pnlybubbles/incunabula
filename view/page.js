cd31bd182f0ebc43f53f9ddbe9eac74b3feb7519
pnlybubbles <pnlybubbles@gmail.com>
2017-07-18 23:49:36 +0900

    fix: glitch of preview due to abandoned v-dom caching

--- view/page.js
+++ view/page.js
@@ -3,7 +3,7 @@ const html = require('choo/html')
 module.exports = (page, isCurrentPage, emit) => {
   return html`
     <section class="sheet ${isCurrentPage ? '' : 'hide'}">
-      ${page.html}
+      ${page.html.map(v => v.cloneNode(true))}
     </section>
   `
 }
