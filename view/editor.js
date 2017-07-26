9a45262d9f714fea2dea091e3ff422afd7d4eabd
pnlybubbles <pnlybubbles@gmail.com>
2017-07-26 21:49:31 +0900

    fix: default behavior when file is dropped

--- view/editor.js
+++ view/editor.js
@@ -3,8 +3,18 @@ const keys = require('../src/keys')
 
 module.exports = (editor, emit) => {
   return html`
-    <div id="editor" class="main noprint">
+    <div
+      id="editor"
+      class="main noprint"
+      ondrop=${drop}
+      ondragenter=${dragHover}
+    >
       <div class="titlebar-gradient"></div>
+      <div
+        class="dropper ${editor.hover ? 'active' : ''}"
+        ondragleave=${dragUnhover}
+        ondragend=${dragUnhover}
+      ></div>
       <textarea
         oninput=${updateText}
         onkeyup=${moveCursor}
@@ -20,9 +30,25 @@ module.exports = (editor, emit) => {
 
   function moveCursor (e) {
     const cursor = {
-      line: e.target.value.slice(0, e.target.selectionStart).split('\n').length
+      line: e.target.value.slice(0, e.target.selectionStart).split('\n').length,
+      start: e.target.selectionStart,
+      end: e.target.selectionEnd
     }
     emit(keys.editor.cursor, cursor)
     emit(keys.viewer.syncPage, cursor)
+  }
+
+  function dragHover (e) {
+    emit(keys.editor.hover, true)
+  }
+
+  function dragUnhover (e) {
+    emit(keys.editor.hover, false)
+  }
+
+  function drop (e) {
+    dragUnhover(e)
+    console.log(e.dataTransfer.files);
+    emit(keys.editor.drop, Array.from(e.dataTransfer.files))
   }
 }
