cf3ff05b50f31e8581a0b89550cdd9ec2a225757
pnlybubbles <pnlybubbles@gmail.com>
2017-08-04 16:39:53 +0900

    feat: add tombow

--- view/page.js
+++ view/page.js
@@ -3,6 +3,14 @@ const html = require('choo/html')
 module.exports = (page, isCurrentPage, emit) => {
   return html`
     <section class="sheet markdown-body ${isCurrentPage ? '' : 'hide'}">
+      <div class="tombow with-left left top"></div>
+      <div class="tombow with-top left top"></div>
+      <div class="tombow with-right right top"></div>
+      <div class="tombow with-top right top"></div>
+      <div class="tombow with-right right bottom"></div>
+      <div class="tombow with-bottom right bottom"></div>
+      <div class="tombow with-left left bottom"></div>
+      <div class="tombow with-bottom left bottom"></div>
       ${page.html.map(v => v.cloneNode(true))}
     </section>
   `
